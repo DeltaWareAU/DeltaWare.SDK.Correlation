387007b3f154779edcdc5d707351e02a977d2443
Bradley Clarke <bradley.clarke@outlook.com>
2022-11-16 16:26:21 +1100

    ADDED | Unit Tests for NServiceBus

---
+++ tests/DeltaWare.SDK.Correlation.NServiceBus.Tests/Mocking/MockContextScopeSetter`.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using DeltaWare.SDK.Correlation.Context.Accessors;
+using DeltaWare.SDK.Correlation.Context.Scope;
+
+namespace DeltaWare.SDK.Correlation.NServiceBus.Tests.Mocking
+{
+    /// <inheritdoc cref="IContextAccessor{TContext}"/>
+    public class MockContextScope<TContext> : IContextAccessor<TContext>, IContextScopeSetter<TContext> where TContext : class
+    {
+        private IContextScope<TContext>? _internalScope = null;
+
+        /// <inheritdoc/>
+        public IContextScope Scope => _internalScope;
+
+        /// <inheritdoc/>
+        public TContext Context => _internalScope.Context;
+
+        public void SetScope(IContextScope<TContext> contextScope)
+            => _internalScope = contextScope;
+    }
+}
---
+++ tests/DeltaWare.SDK.Correlation.NServiceBus.Tests/RetrieveCorrelationIdBehaviorShould.cs
@@ -0,0 +1,119 @@
+﻿using DeltaWare.SDK.Correlation.Context;
+using DeltaWare.SDK.Correlation.NServiceBus.Behaviors;
+using DeltaWare.SDK.Correlation.Options;
+using DeltaWare.SDK.Correlation.Providers;
+using Moq;
+using NServiceBus.Testing;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DeltaWare.SDK.Correlation.Context.Scope;
+using DeltaWare.SDK.Correlation.NServiceBus.Tests.Mocking;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace DeltaWare.SDK.Correlation.NServiceBus.Tests
+{
+    public class RetrieveCorrelationIdBehaviorShould
+    {
+        [Fact]
+        public async Task Retrieve_CorrelationId()
+        {
+            string correlationId = Guid.NewGuid().ToString();
+            string key = "my-test-key";
+
+            MockContextScope<CorrelationContext> contextScope = new MockContextScope<CorrelationContext>();
+
+            Mock<IIdProvider<CorrelationContext>> mockIdProvider = new Mock<IIdProvider<CorrelationContext>>();
+            Mock<IOptions<CorrelationContext>> mockOptions = new Mock<IOptions<CorrelationContext>>();
+
+            mockOptions.Setup(p => p.Key).Returns(key);
+            mockOptions.Setup(p => p.IsRequired).Returns(true);
+
+            RetrieveContextIdBehavior<CorrelationContext> behavior = new RetrieveCorrelationIdBehavior(contextScope, mockIdProvider.Object, mockOptions.Object);
+
+            TestableIncomingPhysicalMessageContext context = new TestableIncomingPhysicalMessageContext
+            {
+                MessageHeaders = new Dictionary<string, string>
+                {
+                    { key, correlationId }
+                }
+            };
+            
+            await behavior.Invoke(context, () => Task.CompletedTask);
+            
+            contextScope.Context.CorrelationId.ShouldBe(correlationId);
+
+            mockIdProvider.Verify(m => m.GenerateId(), Times.Never);
+        }
+
+        [Fact]
+        public async Task Generate_CorrelationId()
+        {
+            string correlationId = Guid.NewGuid().ToString();
+            string key = "my-test-key";
+
+            MockContextScope<CorrelationContext> contextScope = new MockContextScope<CorrelationContext>();
+
+            Mock<IIdProvider<CorrelationContext>> mockIdProvider = new Mock<IIdProvider<CorrelationContext>>();
+            Mock<IOptions<CorrelationContext>> mockOptions = new Mock<IOptions<CorrelationContext>>();
+
+            mockIdProvider.Setup(m => m.GenerateId()).Returns(correlationId);
+
+            mockOptions.Setup(p => p.Key).Returns(key);
+            mockOptions.Setup(p => p.IsRequired).Returns(false);
+
+            RetrieveContextIdBehavior<CorrelationContext> behavior = new RetrieveCorrelationIdBehavior(contextScope, mockIdProvider.Object, mockOptions.Object);
+
+            TestableIncomingPhysicalMessageContext context = new TestableIncomingPhysicalMessageContext();
+            
+            await behavior.Invoke(context, () => Task.CompletedTask);
+            
+            contextScope.Context.CorrelationId.ShouldBe(correlationId);
+
+            mockIdProvider.Verify(m => m.GenerateId(), Times.Once);
+        }
+
+        [Fact]
+        public async Task Begin_LoggingScope()
+        {
+            string correlationId = Guid.NewGuid().ToString();
+            string key = "my-test-key";
+            string loggingScopeKey = "my-test-logging-scope-key";
+
+            MockContextScope<CorrelationContext> contextScope = new MockContextScope<CorrelationContext>();
+
+            Mock<IIdProvider<CorrelationContext>> mockIdProvider = new Mock<IIdProvider<CorrelationContext>>();
+            Mock<IOptions<CorrelationContext>> mockOptions = new Mock<IOptions<CorrelationContext>>();
+            Mock<ILogger<CorrelationContext>> mockLogger = new Mock<ILogger<CorrelationContext>>();
+
+            mockOptions.Setup(p => p.Key).Returns(key);
+            mockOptions.Setup(p => p.IsRequired).Returns(true);
+            mockOptions.Setup(p => p.AttachToLoggingScope).Returns(true);
+            mockOptions.Setup(p => p.LoggingScopeKey).Returns(loggingScopeKey);
+
+            mockLogger.Setup(m => m.BeginScope(It.IsAny<Dictionary<string, string>>())).Returns(new MockDisposable());
+
+
+            RetrieveContextIdBehavior<CorrelationContext> behavior = new RetrieveCorrelationIdBehavior(contextScope, mockIdProvider.Object, mockOptions.Object, mockLogger.Object);
+
+            TestableIncomingPhysicalMessageContext context = new TestableIncomingPhysicalMessageContext
+            {
+                MessageHeaders = new Dictionary<string, string>
+                {
+                    { key, correlationId }
+                }
+            };
+
+            await behavior.Invoke(context, () => Task.CompletedTask);
+
+            contextScope.Context.CorrelationId.ShouldBe(correlationId);
+
+            mockIdProvider.Verify(m => m.GenerateId(), Times.Never);
+            mockLogger.Verify(m => m.BeginScope(It.IsAny<Dictionary<string, string>>()), Times.Once);
+            mockLogger.Verify(m => m.BeginScope(It.Is<Dictionary<string, string>>(v => v.ContainsKey(loggingScopeKey) && v[loggingScopeKey] == correlationId)), Times.Once);
+        }
+    }
+}
